7fe00145657a16a53c89719a4ce3ca8f2b7a8136
DonaldMcGallavry <167004436+DonaldMcGallavry@users.noreply.github.com>
2024-06-02 18:51:00 +0300

    Add files via upload

---
+++ LabProjectControl/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LabProjectControl
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите Периметр");
+            double p = double.Parse(Console.ReadLine());
+            double a = p / 3;
+            double p2 = p / 2;
+            double s = Math.Sqrt(p2 * (p2 - a) * (p2 - a) * (p2 - a));
+            Console.WriteLine($"Сторона Площадь \v {a:F2}   {s:F2}");
+        }
+    }
+}
