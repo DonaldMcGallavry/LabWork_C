7bbfab1248a397072a7b1a6a33a6fae72f46911e
DonaldMcGallavry <167004436+DonaldMcGallavry@users.noreply.github.com>
2024-06-03 17:37:36 +0300

    Add files via upload
    Лабораторная робота 2

---
+++ DistanceLab2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DistanceLab2
+{
+    public struct Distance
+    {
+        public double F;
+        public double D;
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Distance First, Second, Third;
+            First.F = double.Parse(Console.ReadLine());
+            First.D = double.Parse(Console.ReadLine());
+            Second.F = double.Parse(Console.ReadLine());
+            Second.D = double.Parse(Console.ReadLine());
+            Third.F = (First.F + Second.F) + (int)(First.D + Second.D)/12;
+            Third.D = (First.D + Second.D)%12;
+            Console.WriteLine($"Расстояние {Third.F}' - {Third.D}''");
+        }
+    }
+}
